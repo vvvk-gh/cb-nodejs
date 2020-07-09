ad8ca595cd85e17283b0b17703fa9a123b89b073
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-09 13:58:45 +0530

    assignment

---
+++ nodejs/r_w_assignment/readfile.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const data = fs.readFileSync(__dirname + '/readfromhere.txt' )
+
+const lines = data.toString().split("\n")
+console.log(lines);
+
+let unique = [...new Set(lines)];
+console.log(unique);
+
+let resArray = unique.map((item) => [item +" "+lines.filter(val => val == item).length])
+resArray = [...resArray].toString().split(",").join("\n")
+console.log(resArray);
+
+    fs.writeFile(__dirname + "/writehere.txt" , resArray , (err) =>{
+            if (err) throw err
+            console.log("copied into the file")
+            
+        })
