4b3897fdc0eb8d1c075472c3a0bef810d011b238
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-10 03:16:06 +0530

    handlebars:hbs , app.render

--- todolist/server.js
+++ todolist/server.js
@@ -1,30 +1,17 @@
-const express = require('express');
+ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({extented:true}))
 
-let tasks =[
-    'simple random task'
-]
+app.set('view engine', 'hbs');
+let tasks = ["simpledata"]
 
 app.get('/' , (req, res)=>{
-
-    let taskList = tasks.map((t) => `<li>${t}</li>`)
-    res.send(`
-    <html>
-        <body>
-            <form action="/" method ="POST">
-            <input name="newtask">
-            <button type="submit">Add</button>
-            </form>
-            <ul>
-                ${taskList.join('\n')}
-            </ul>
-        </body>
-    </html>
-    `)
+    res.render('home', {
+        title : "Todo List",
+        tasks
+    })
 })
-
 
 app.post("/" , (req, res)=>{
     tasks.push(req.body.newtask)
