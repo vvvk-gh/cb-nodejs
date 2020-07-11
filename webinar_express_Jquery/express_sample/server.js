ef85c7cfadcc67b8b3bd446a764acba2fba8aeff
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-11 22:56:31 +0530

    todo list

--- webinar_express_Jquery/express_sample/server.js
+++ webinar_express_Jquery/express_sample/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
 server = express();
+
+//we can use static files in express using express.static 
+server.use('/public' , express.static(__dirname +"/public"))
 
 server.get("/hello" , (req , res)=>{
     res.send("Hello");
