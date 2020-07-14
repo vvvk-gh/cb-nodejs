3f12becf736b99764a93cf0d427c4ebd26525b86
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-15 00:00:44 +0530

    sequelize connection

--- sequelize/connect.js
+++ sequelize/connect.js
@@ -1,10 +1,14 @@
+//importing the sequlize
 const Sequelize = require('sequelize')
 
+//creating a connection 
+//it has 3 parameters db , user , password , object { host and dialect details }
 const db = new Sequelize('sampledb1' , 'sampleuser1' , 'samplepass1' ,{
     host: 'localhost',
     dialect: 'mysql' 
 })
 
+//verifying the connection using authenticate method
 db.authenticate()
     .then(()=> console.log('Connection Worked'))
     .catch((err)=> console.log(err))
