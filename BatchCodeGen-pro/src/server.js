189451dbd8e0617b11174e2b5d752e0cca9469bf
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-07-18 22:42:03 +0530

    express and hbs

--- BatchCodeGen-pro/src/server.js
+++ BatchCodeGen-pro/src/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const {Center , Season, Course} = require('./db/models')
+const {Center, Season, Course, Batch} = require('./db/models')
 
 app.set('view engine', 'hbs')
 app.set("views" , path.resolve(__dirname ,'../views'))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 app.get('/batchcode' , async (req , res)=>{
   try {
@@ -24,6 +27,41 @@ app.get('/batchcode' , async (req , res)=>{
 })
 
 
-module.exports = {
+app.post('/batchcode' , async (req ,res)=>{
+    let batchcode = ''; //WDPP18S1
+    batchcode += req.body.course
+    batchcode += req.body.center
+    batchcode += req.body.year.substr(2)
+    batchcode += req.body.season
+    batchcode += req.body.batchno
+    
+  try {
+        const batch = await Batch.create({
+            code :batchcode,
+            year : req.body.year,
+            courseId: req.body.course,
+            centerId: req.body.center,
+            seasonId: req.body.season,
+            start: Date.parse(req.body.start),
+            end : Date.parse(req.body.end) 
+          })
+        res.send(batch.code);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+app.get('/batches' , async(req ,res)=>{
+
+    const batches = await Batch.findAll({
+      include : [Course , Season , Center]
+    })
+
+  res.render("batches" , {batches})
+})
+
+
+exports = module.exports = {
   app          
 }
